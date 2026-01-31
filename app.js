c3ab34773f5afd29aea7e2b8d6a1dda0adc5c1b0
apierre1541 <axelle.pierre@btsinfo-aurillac.fr>
2026-01-31 15:32:21 +0100

    Modification du fichier app.js pour ajouter le lien à notre base de données (mangoDB)

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://axpierrelim_db_user:mango@cluster0.lrrdd5t.mongodb.net/maxpierrelim_db_user?appName=Cluster0");
+
 const methodOverride = require("method-override");
 const flash = require("connect-flash");
 
 app.set("view engine", "ejs");
 //app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:false}));
+
 
 app.get("/", function(req, res){
     res.render("Page_accueil");
@@ -15,3 +19,4 @@ app.get("/", function(req, res){
 app.listen(3000, function(req, res){
     console.log("tout marche bien!");
 })
+
