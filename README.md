60a621767f3cf4e223a4a70cfb8df04491ba43c8
apierre1541 <axelle.pierre@btsinfo-aurillac.fr>
2026-01-31 16:01:42 +0100

    Modification du fichier Page_accueil.ejs + ajout du fichier inscription.ejs + modification du fichier app.js et création du dossier models pour ajouter le fichier users.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GroupeA-S4-L315-2026
+
+A rajouter dans mongoose si erreur : 
+, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const app = express();
+
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+
 mongoose.connect("mongodb+srv://axpierrelim_db_user:mango@cluster0.lrrdd5t.mongodb.net/maxpierrelim_db_user?appName=Cluster0");
+app.set("view engine", "ejs");
+
+//app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+const User = require("./models/user")
 
 const methodOverride = require("method-override");
 const flash = require("connect-flash");
 
-app.set("view engine", "ejs");
-//app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:false}));
-
-
 app.get("/", function(req, res){
     res.render("Page_accueil");
-})
+});
+
+app.get("/inscription", function(req, res){
+    res.render("inscription")
+});
+
 app.listen(3000, function(req, res){
     console.log("tout marche bien!");
 })
